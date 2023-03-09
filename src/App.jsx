337ad9c36b8bc399283ecb80a1b560e5c2ca9b2c
⚜️  X  ⚜️  U  ⚜️  D  ⚜️ O  ⚜️  B  ⚜️  E  ⚜️  R  ⚜️  D  ⚜️  I  ⚜️ <121534521+xudoberdi2008@users.noreply.github.com>
2023-03-09 14:28:50 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
 
 function App() {
-
-  const [posts, setPosts] = useState([])
-
-
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-      .then(res => {
-        setPosts(res.data)
-        //  console.log(res.data)
-      })
-  }, [])
 
   // console.log();
 
@@ -24,36 +12,7 @@ function App() {
       <h1>Hello</h1>
       <p>shakalad</p>
       <b>hgfhsgdfhdsgfh</b>
-      {/* {
-        posts.map(post =>(
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <h1>{post.body}</h1>
-          </div>
-        ))
-      } */}
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            posts.map(post => (
-              <tr key={post.id}>
-                <th scope='row'>{post.id}</th>
-                <td>{post.title}</td>
-                <td>{post.body}</td>
 
-              </tr>
-            ))
-          }
-
-        </tbody>
-      </table>
     </div>
   );
 }
